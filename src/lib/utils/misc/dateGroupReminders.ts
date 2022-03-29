1c8180f251fe668d579cc6071c4ee96fc2c44708
jakubblaha-ids <jakubblaha@ids.company>
2022-03-29 19:54:33 +0200

    feat: humanize reminder day labels

--- src/lib/utils/misc/dateGroupReminders.ts
+++ src/lib/utils/misc/dateGroupReminders.ts
@@ -5,8 +5,22 @@ import dayjs from 'dayjs';
 export function dateGroupReminders(reminders: ReminderData[]) {
 	const groups: DateGroupData[] = [];
 
+	const today = dayjs().startOf('day');
+
 	for (const reminder of reminders) {
-		const groupTitle = dayjs.unix(reminder.timestamp).format('MMM DD, YYYY');
+		const reminderDay = dayjs.unix(reminder.timestamp).startOf('day');
+
+		let groupTitle: string;
+
+		if (today.toISOString() === reminderDay.toISOString()) {
+			groupTitle = 'Today';
+		} else if (today.add(1, 'day').toISOString() === reminderDay.toISOString()) {
+			groupTitle = 'Tomorrow';
+		} else {
+			groupTitle = reminderDay.format('dddd, MMMM D');
+		}
+
+		// const groupTitle = .format('MMM DD, YYYY');
 
 		const group = groups.find((group) => group.title === groupTitle);
 
