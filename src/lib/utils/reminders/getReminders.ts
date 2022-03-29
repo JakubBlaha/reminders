82501bf9f7211422e71fc913c97d87a7b1d4fdb4
jakubblaha-ids <jakubblaha@ids.company>
2022-03-29 20:04:03 +0200

    fix: order reminders by date

--- src/lib/utils/reminders/getReminders.ts
+++ src/lib/utils/reminders/getReminders.ts
@@ -1,13 +1,17 @@
 import { get } from 'svelte/store';
 import { userId } from '$lib/utils/auth';
-import { collection, getDocs, query, where } from 'firebase/firestore';
+import { collection, getDocs, orderBy, query, where } from 'firebase/firestore';
 import { db } from '$lib/utils/misc/firebase';
 import type { ReminderData } from '$lib/interfaces/ReminderData';
 
 export async function getReminders() {
 	const uid = get(userId);
 
-	const q = query(collection(db, 'reminders'), where('creatorId', '==', uid));
+	const q = query(
+		collection(db, 'reminders'),
+		where('creatorId', '==', uid),
+		orderBy('timestamp', 'asc')
+	);
 	const snap = await getDocs(q);
 
 	return snap.docs.map((doc) => ({ id: doc.id, ...doc.data() })) as ReminderData[];
