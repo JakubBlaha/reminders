cd7b3eeac4948e6659e37f82bc0cc67b52d76262
jakubblaha-ids <jakubblaha@ids.company>
2022-03-30 19:30:00 +0200

    feat: reminded artifact

--- src/lib/components/ReminderCard/ReminderCard.svelte
+++ src/lib/components/ReminderCard/ReminderCard.svelte
@@ -14,7 +14,7 @@
 	class="flex items-center w-full h-16 p-4 mt-2 transition-all rounded cursor-pointer bg-neutral-800 hover:pl-6 active:pl-2"
 	on:click={clickCard}
 >
-	<div>{reminderData.title}</div>
+	<div class={reminderData.reminded && 'line-through opacity-30'}>{reminderData.title}</div>
 	<div class="flex-grow" />
 	<div class="text-neutral-500">{timeFromTimestamp(reminderData.timestamp)}</div>
 </button>
